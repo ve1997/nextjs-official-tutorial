8875145f259245bdd49bab5452e0b3597f4cba41
ve1997 <83203852+ve1997@users.noreply.github.com>
2024-09-21 14:50:32 +0900

    feat: LatestInvoicesをReact.Suspenseでラップ

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,13 +1,15 @@
-import { fetchCardData, fetchLatestInvoices } from "@/app/lib/data";
+import { fetchCardData } from "@/app/lib/data";
 import { Card } from "@/app/ui/dashboard/cards";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import { lusitana } from "@/app/ui/fonts";
-import { RevenueChartSkeleton } from "@/app/ui/skeletons";
+import {
+	LatestInvoicesSkeleton,
+	RevenueChartSkeleton,
+} from "@/app/ui/skeletons";
 import { Suspense } from "react";
 
 export default async function Page() {
-	const latestInvoices = await fetchLatestInvoices();
 	const {
 		totalPaidInvoices,
 		totalPendingInvoices,
@@ -33,7 +35,9 @@ export default async function Page() {
 				<Suspense fallback={<RevenueChartSkeleton />}>
 					<RevenueChart />
 				</Suspense>
-				<LatestInvoices latestInvoices={latestInvoices} />
+				<Suspense fallback={<LatestInvoicesSkeleton />}>
+					<LatestInvoices />
+				</Suspense>
 			</div>
 		</main>
 	);
