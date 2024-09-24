ded64e8993273613ef49cf90e9d42b4ba694b4a9
ve1997 <83203852+ve1997@users.noreply.github.com>
2024-09-24 19:21:36 +0900

    feat: URLクエリパラメータを更新する

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,9 +1,20 @@
 "use client";
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function Search({ placeholder }: { placeholder: string }) {
+	const searchParams = useSearchParams();
+	const pathname = usePathname();
+	const { replace } = useRouter();
 	function handleSearch(term: string) {
+		const params = new URLSearchParams(searchParams);
+		if (term) {
+			params.set("query", term);
+		} else {
+			params.delete("query");
+		}
+		replace(`${pathname}?${params.toString()}`);
 		console.log("🚀", term);
 	}
 	return (
