034e6f709395ddce2aa66df0aadc31f95290fcff
ve1997 <83203852+ve1997@users.noreply.github.com>
2024-09-24 19:45:48 +0900

    feat: リクエストとテーブルの更新

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,7 +6,17 @@ import Search from "@/app/ui/search";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 
-export default async function Page() {
+export default async function Page({
+	searchParams,
+}: {
+	searchParams?: {
+		query?: string;
+		page?: string;
+	};
+}) {
+	const query = searchParams?.query || "";
+	const currentPage = Number(searchParams?.page) || 1;
+
 	return (
 		<div className="w-full">
 			<div className="flex w-full items-center justify-between">
@@ -20,9 +30,9 @@ export default async function Page() {
 				</Suspense>
 				<CreateInvoice />
 			</div>
-			{/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-        <Table query={query} currentPage={currentPage} />
-      </Suspense> */}
+			<Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+				<Table query={query} currentPage={currentPage} />
+			</Suspense>
 			<div className="mt-5 flex w-full justify-center">
 				{/* <Pagination totalPages={totalPages} /> */}
 			</div>
