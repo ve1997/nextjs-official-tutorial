c97517a13e2586c85591eb9df6eee989ea4701fd
ve1997 <83203852+ve1997@users.noreply.github.com>
2024-09-27 18:05:28 +0900

    feat: データベースにデータを挿入

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -1,4 +1,6 @@
 "use server";
+
+import { sql } from "@vercel/postgres";
 import { z } from "zod";
 
 const FormSchema = z.object({
@@ -22,4 +24,9 @@ export async function createInvoice(formData: FormData) {
 	});
 	const amountInCents = amount * 100;
 	const date = new Date().toISOString().split("T")[0];
+
+	await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
 }
