c91ea7c96d1d014c4758ad5cf1d84ed38ce9a271
ve1997 <83203852+ve1997@users.noreply.github.com>
2024-09-30 10:23:40 +0900

    feat: Googleプロバイダーで実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import { lusitana } from "@/app/ui/fonts";
+import { signIn } from "@/auth";
 // import styles from "@/app/ui/home.module.css";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
-import Link from "next/link";
+import { Button } from "./ui/button";
 
 export default function Page() {
 	return (
@@ -26,12 +27,16 @@ export default function Page() {
 						</a>
 						, brought to you by Vercel.
 					</p>
-					<Link
-						href="/login"
-						className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+					<form
+						action={async () => {
+							"use server";
+							await signIn("google");
+						}}
 					>
-						<span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-					</Link>
+						<Button className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base">
+							<span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+						</Button>
+					</form>
 				</div>
 				<div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
 					{/* Add Hero Images Here */}
