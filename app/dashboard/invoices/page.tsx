b8344994885755f4accc55fdf9569dce136da4ce
ve1997 <83203852+ve1997@users.noreply.github.com>
2024-09-30 09:51:15 +0900

    feat: テンプレートを定義する

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -9,7 +9,7 @@ import { Metadata } from "next";
 import { Suspense } from "react";
 
 export const metadata: Metadata = {
-	title: "Invoices | Acme Dashboard",
+	title: "Invoices",
 };
 
 export default async function Page({
